7ece446c83b5ec6d997a0b1d5173cf37609d9841
Rayan Levert <rayanlevert@msn.com>
2025-04-11 20:03:12 +0200

    Columns\Enum : added an enum with all columns type defined for mostly all PDOs

---
+++ src/Columns/Type.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace RayanLevert\Model\Columns;
+
+/** Enum for common database column types used in PDO. This enum defines standard SQL data types and their corresponding PHP types. */
+enum Type: string
+{
+    /** Integer type for primary keys and foreign keys. */
+    case INTEGER = 'INT';
+
+    /** Unsigned integer type for IDs and positive numbers. */
+    case UNSIGNED_INTEGER = 'INT UNSIGNED';
+
+    /** Auto-incrementing integer for primary keys. */
+    case AUTO_INCREMENT = 'INT UNSIGNED AUTO_INCREMENT PRIMARY KEY';
+
+    /** Small integer type for flags and status codes. */
+    case SMALL_INTEGER = 'SMALLINT';
+
+    /** Tiny integer type for boolean values. */
+    case TINY_INTEGER = 'TINYINT';
+
+    /** Boolean type (TINYINT(1)). */
+    case BOOLEAN = 'TINYINT(1)';
+
+    /** Decimal type for precise numbers. */
+    case DECIMAL = 'DECIMAL';
+
+    /** Float type for approximate numbers. */
+    case FLOAT = 'FLOAT';
+
+    /** Double precision floating point. */
+    case DOUBLE = 'DOUBLE';
+
+    /** Character type with fixed length. */
+    case CHAR = 'CHAR';
+
+    /** Variable-length character string. */
+    case VARCHAR = 'VARCHAR';
+
+    /** Text type for longer strings. */
+    case TEXT = 'TEXT';
+
+    /** Medium text type for very long strings. */
+    case MEDIUM_TEXT = 'MEDIUMTEXT';
+
+    /** Long text type for extremely long strings. */
+    case LONG_TEXT = 'LONGTEXT';
+
+    /** Date type for storing dates. */
+    case DATE = 'DATE';
+
+    /** Time type for storing times. */
+    case TIME = 'TIME';
+
+    /** Datetime type for storing date and time. */
+    case DATETIME = 'DATETIME';
+
+    /** Timestamp type for storing date and time with timezone. */
+    case TIMESTAMP = 'TIMESTAMP';
+
+    /** Binary type for storing binary data. */
+    case BINARY = 'BINARY';
+
+    /** VarBinary type for storing variable-length binary data. */
+    case VARBINARY = 'VARBINARY';
+
+    /** Blob type for storing large binary objects. */
+    case BLOB = 'BLOB';
+
+    /** MediumBlob type for storing medium-sized binary objects. */
+    case MEDIUM_BLOB = 'MEDIUMBLOB';
+
+    /** LongBlob type for storing large binary objects. */
+    case LONG_BLOB = 'LONGBLOB';
+
+    /** JSON type for storing JSON data. */
+    case JSON = 'JSON';
+
+    /** Get the PHP type corresponding to this SQL type. */
+    public function getPhpType(): string
+    {
+        return match($this) {
+            self::INTEGER, self::UNSIGNED_INTEGER, self::AUTO_INCREMENT, 
+            self::SMALL_INTEGER, self::TINY_INTEGER => 'int',
+            self::BOOLEAN => 'bool',
+            self::DECIMAL, self::FLOAT, self::DOUBLE => 'float',
+            self::CHAR, self::VARCHAR, self::TEXT, self::MEDIUM_TEXT, self::LONG_TEXT, => 'string',
+            self::DATE, self::TIME, self::DATETIME, self::TIMESTAMP => 'DateTimeInterface',
+            self::BINARY, self::VARBINARY, self::BLOB, self::MEDIUM_BLOB, 
+            self::LONG_BLOB => 'string',
+            self::JSON => 'mixed',
+        };
+    }
+} 
---
+++ tests/Columns/TypeTest.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace RayanLevert\Model\Tests\Columns;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use RayanLevert\Model\Columns\Type;
+
+#[CoversClass(Type::class)]
+class TypeTest extends \PHPUnit\Framework\TestCase
+{
+    #[Test]
+    public function integerType(): void
+    {
+        $this->assertSame('INT', Type::INTEGER->value);
+        $this->assertSame('int', Type::INTEGER->getPhpType());
+    }
+
+    #[Test]
+    public function unsignedIntegerType(): void
+    {
+        $this->assertSame('INT UNSIGNED', Type::UNSIGNED_INTEGER->value);
+        $this->assertSame('int', Type::UNSIGNED_INTEGER->getPhpType());
+    }
+
+    #[Test]
+    public function autoIncrementType(): void
+    {
+        $this->assertSame('INT UNSIGNED AUTO_INCREMENT PRIMARY KEY', Type::AUTO_INCREMENT->value);
+        $this->assertSame('int', Type::AUTO_INCREMENT->getPhpType());
+    }
+
+    #[Test]
+    public function booleanType(): void
+    {
+        $this->assertSame('TINYINT(1)', Type::BOOLEAN->value);
+        $this->assertSame('bool', Type::BOOLEAN->getPhpType());
+    }
+
+    #[Test]
+    public function varcharType(): void
+    {
+        $this->assertSame('VARCHAR', Type::VARCHAR->value);
+        $this->assertSame('string', Type::VARCHAR->getPhpType());
+    }
+
+    #[Test]
+    public function textType(): void
+    {
+        $this->assertSame('TEXT', Type::TEXT->value);
+        $this->assertSame('string', Type::TEXT->getPhpType());
+    }
+
+    #[Test]
+    public function timestampType(): void
+    {
+        $this->assertSame('TIMESTAMP', Type::TIMESTAMP->value);
+        $this->assertSame('DateTimeInterface', Type::TIMESTAMP->getPhpType());
+    }
+
+    #[Test]
+    public function jsonType(): void
+    {
+        $this->assertSame('JSON', Type::JSON->value);
+        $this->assertSame('mixed', Type::JSON->getPhpType());
+    }
+
+    #[Test]
+    #[DataProvider('provideNumericTypes')]
+    public function numericTypesReturnIntPhpType(Type $type): void
+    {
+        $this->assertSame('int', $type->getPhpType());
+    }
+
+    #[Test]
+    #[DataProvider('provideStringTypes')]
+    public function stringTypesReturnStringPhpType(Type $type): void
+    {
+        $this->assertSame('string', $type->getPhpType());
+    }
+
+    #[Test]
+    #[DataProvider('provideDateTimeTypes')]
+    public function dateTimeTypesReturnDateTimeInterfacePhpType(Type $type): void
+    {
+        $this->assertSame('DateTimeInterface', $type->getPhpType());
+    }
+
+    public static function provideNumericTypes(): array
+    {
+        return [
+            'INTEGER'           => [Type::INTEGER],
+            'UNSIGNED_INTEGER'  => [Type::UNSIGNED_INTEGER],
+            'AUTO_INCREMENT'    => [Type::AUTO_INCREMENT],
+            'SMALL_INTEGER'     => [Type::SMALL_INTEGER],
+            'TINY_INTEGER'      => [Type::TINY_INTEGER]
+        ];
+    }
+
+    public static function provideStringTypes(): array
+    {
+        return [
+            'CHAR'          => [Type::CHAR],
+            'VARCHAR'       => [Type::VARCHAR],
+            'TEXT'          => [Type::TEXT],
+            'MEDIUM_TEXT'   => [Type::MEDIUM_TEXT],
+            'LONG_TEXT'     => [Type::LONG_TEXT],
+            'BINARY'        => [Type::BINARY],
+            'VARBINARY'     => [Type::VARBINARY],
+            'BLOB'          => [Type::BLOB],
+            'MEDIUM_BLOB'   => [Type::MEDIUM_BLOB],
+            'LONG_BLOB'     => [Type::LONG_BLOB]
+        ];
+    }
+
+    public static function provideDateTimeTypes(): array
+    {
+        return [
+            'DATE'      => [Type::DATE],
+            'TIME'      => [Type::TIME],
+            'DATETIME'  => [Type::DATETIME],
+            'TIMESTAMP' => [Type::TIMESTAMP]
+        ];
+    }
+} 
